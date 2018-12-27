ccab8bdc43864cb10d40a0c62e99e4017e906e48
Will <will@obo.pm>
2018-12-27 14:05:41 -0800

    Updating .gitignore for globally ignoring

--- testing-ar-tuts/src/components/__tests__/App.test.js
+++ testing-ar-tuts/src/components/__tests__/App.test.js
@@ -6,10 +6,12 @@ import App from '../App';
 it('shows a comment box',()=>{
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+
   //Looks inside the div
   // and checks to see if the CommentBos is in there
   //console.log('[App.test.js] -- =', div.innerHTML);
   expect(div.innerHTML).toContain('Comment Box');
+
 
   ReactDOM.unmountComponentAtNode(div);
 });
